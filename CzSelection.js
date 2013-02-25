c5773465c43b9b7e94ed9f5a066c85baf1098067
Jan Pecha <janpecha@email.cz>
2013-02-25 13:05:00 +0100

    added method getLines()
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- CzSelection.js
+++ CzSelection.js
@@ -1,7 +1,7 @@
 /** Cz Selection Library
  * @author		Jan Pecha <janpecha@email.cz>, 2013
  * @license		New BSD License
- * @version		2013-02-25-3
+ * @version		2013-02-25-4
  */
 
 var Cz = Cz || {};
@@ -9,6 +9,15 @@ Cz.Selection = Cz.Selection || {};
 
 Cz.Selection.getText = function (textarea) {
 	return textarea.value.substring(this.getStartPos(textarea), this.getEndPos(textarea));
+};
+
+
+Cz.Selection.getLines = function (textarea) {
+	var selEndPos = this.getEndPos(textarea);
+	var startPos = textarea.value.substring(0, this.getStartPos(textarea)).lastIndexOf("\n");
+	var endPos = selEndPos + textarea.value.substring(selEndPos).indexOf("\n");
+	
+	return textarea.value.substring(startPos + 1, endPos).replace("\r", '').split("\n");
 };
 
 
