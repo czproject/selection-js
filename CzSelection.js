22ae670bd4fc8f3ed12fff72a2b5e3942bb82653
Jan Pecha <janpecha@email.cz>
2013-02-25 12:27:54 +0100

    added methods getStartPos, getEndPos & getLength
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- CzSelection.js
+++ CzSelection.js
@@ -1,13 +1,28 @@
 /** Cz Selection Library
  * @author		Jan Pecha <janpecha@email.cz>, 2013
  * @license		New BSD License
- * @version		2013-02-25-1
+ * @version		2013-02-25-2
  */
 
 var Cz = Cz || {};
 Cz.Selection = Cz.Selection || {};
 
 Cz.Selection.getText = function (textarea) {
-	return textarea.value.substr(textarea.selectionStart, (textarea.selectionEnd - textarea.selectionStart));
+	return textarea.value.substr(start = this.getStartPos(textarea), this.getEndPos(textarea) - start);
+};
+
+
+Cz.Selection.getStartPos = function (textarea) {
+	return textarea.selectionStart;
+};
+
+
+Cz.Selection.getEndPos = function (textarea) {
+	return textarea.selectionEnd;
+};
+
+
+Cz.Selection.getLength = function (textarea) {
+	return this.getEndPos(textarea) - this.getStartPos(textarea);
 };
 
