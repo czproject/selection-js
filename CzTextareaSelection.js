01e91e32b47459d78fffd1582692b14211d8c4ad
Jan Pecha <janpecha@email.cz>
2013-08-07 13:44:48 +0200

    Changed name of TextareaSelection to TextAreaSelection
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- CzTextareaSelection.js
+++ CzTextareaSelection.js
@@ -1,60 +1,60 @@
 
 var Cz;
 (function (Cz) {
-	var TextareaSelection = (function () {
-		function TextareaSelection(textarea) {
+	var TextAreaSelection = (function () {
+		function TextAreaSelection(textarea) {
 			this.textarea = textarea;
 		}
-		TextareaSelection.prototype.getText = function () {
+		TextAreaSelection.prototype.getText = function () {
 			return Cz.Selection.getText(this.textarea);
 		};
 
-		TextareaSelection.prototype.getLines = function () {
+		TextAreaSelection.prototype.getLines = function () {
 			return Cz.Selection.getLines(this.textarea);
 		};
 
-		TextareaSelection.prototype.insert = function (text, pos) {
+		TextAreaSelection.prototype.insert = function (text, pos) {
 			return Cz.Selection.insert(this.textarea, text, pos);
 		};
 
-		TextareaSelection.prototype.replace = function (text, alwaysReplace) {
-			return Cz.Selection.replace(this.textarea, text, alwaysReplace);
+		TextAreaSelection.prototype.replace = function (text, alwaysReplace, selectReplacedText) {
+			return Cz.Selection.replace(this.textarea, text, alwaysReplace, selectReplacedText);
 		};
 
-		TextareaSelection.prototype.wrap = function (prefix, suffix) {
-			Cz.Selection.wrap(this.textarea, prefix, suffix);
+		TextAreaSelection.prototype.wrap = function (prefix, suffix, selectAll) {
+			Cz.Selection.wrap(this.textarea, prefix, suffix, selectAll);
 		};
 
-		TextareaSelection.prototype.unwrap = function (prefix, suffix) {
+		TextAreaSelection.prototype.unwrap = function (prefix, suffix) {
 			Cz.Selection.unwrap(this.textarea, prefix, suffix);
 		};
 
-		TextareaSelection.prototype.wrapLines = function (prefix, suffix, ignoreSpaces) {
+		TextAreaSelection.prototype.wrapLines = function (prefix, suffix, ignoreSpaces) {
 			Cz.Selection.wrapLines(this.textarea, prefix, suffix, ignoreSpaces);
 		};
 
-		TextareaSelection.prototype.unwrapLines = function (prefix, suffix, ignoreSpaces) {
+		TextAreaSelection.prototype.unwrapLines = function (prefix, suffix, ignoreSpaces) {
 			Cz.Selection.unwrapLines(this.textarea, prefix, suffix, ignoreSpaces);
 		};
 
-		TextareaSelection.prototype.getStartPos = function () {
+		TextAreaSelection.prototype.getStartPos = function () {
 			return Cz.Selection.getStartPos(this.textarea);
 		};
 
-		TextareaSelection.prototype.getEndPos = function () {
+		TextAreaSelection.prototype.getEndPos = function () {
 			return Cz.Selection.getEndPos(this.textarea);
 		};
 
-		TextareaSelection.prototype.getLength = function () {
+		TextAreaSelection.prototype.getLength = function () {
 			return Cz.Selection.getLength(this.textarea);
 		};
 		
-		TextareaSelection.prototype.focus = function () {
+		TextAreaSelection.prototype.focus = function () {
 			return this.textarea.focus();
 		};
 		
-		return TextareaSelection;
+		return TextAreaSelection;
 	})();
-	Cz.TextareaSelection = TextareaSelection;
+	Cz.TextAreaSelection = TextAreaSelection;
 })(Cz || (Cz = {}));
 
